3ac7e7a8327e2b4e1aed3babc715c95f21f5f448
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-11-30 17:47:33 -0500

    thought model created

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,8 @@ const { Schema, model, Types } = require('mongoose');
 
 //moment imported
 
-const moment = require('moment')
+const moment = require('moment');
+const { kStringMaxLength } = require('buffer');
 
 //Schema- Reaction
 
@@ -17,6 +18,62 @@ const SchemaReaction = new Schema (
             required: true,
             maxlength: 280
         },
-        
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MM DD, YYYY [at] hh:mm a"),
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+            
+        },
+        id: false,
     }
 )
+//Schema - Thought
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlenghth: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [SchemaReaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+thoughtSchema.virtual("reactionCount")
+.get(function() {
+    return this.reactions.length;
+});
+
+//user model created using userschema
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
